bee863df86fcc05f4ea73d6922c8c721380212d1
BharathRamakrishnan <Bharath@Shankars-Mac-mini.local>
2015-08-27 19:10:54 +0530

    fix

--- YuMeTestApp/Scripts/UnitTest.sh
+++ YuMeTestApp/Scripts/UnitTest.sh
@@ -15,19 +15,16 @@ PWORKSPACE=$PROJECT.xcworkspace
 
 echo "<<<<<<<<<<<<<<<<<<<<<<< UNIT TEST START >>>>>>>>>>>>>>>>>>>>>>>>>>>"
 
-	#xcodebuild -sdk iphonesimulator \
-    #       -workspace $WORKSPACE \
-    #       -scheme $SCHEME_NAME \
-    #       -configuration Debug \
-    #       RUN_APPLICATION_TESTS_WITH_IOS_SIM=YES \
-    #       ONLY_ACTIVE_ARCH=NO \
-    #       clean build           
+	xcodebuild -sdk iphonesimulator \
+           -workspace "$WORKSPACE/$PWORKSPACE" \
+           -scheme $SCHEME_NAME \
+           -configuration Debug \
+           RUN_APPLICATION_TESTS_WITH_IOS_SIM=YES \
+           ONLY_ACTIVE_ARCH=NO \
+    	   clean build           
     
 #xctool -workspace "$WORKSPACE/$PWORKSPACE" -scheme $SCHEME_NAME -sdk iphonesimulator8.4 -reporter plain -reporter junit:$TEST_REPORT_FILE test
     
-xctool -workspace /Users/Shared/Jenkins/Home/jobs/YuMeiOSZISUnitTest/workspace/YuMeTestApp/YuMeZISTestApp.xcworkspace -scheme YuMeZISTests -reporter plain  -reporter junit:$TEST_REPORT_FILE test -test-sdk iphonesimulator
-
-
     #-IDECustomDerivedDataLocation=$BUILD_PATH \
 	#GCC_GENERATE_TEST_COVERAGE_FILES=YES GCC_INSTRUMENT_PROGRAM_FLOW_ARCS=YES \
     
