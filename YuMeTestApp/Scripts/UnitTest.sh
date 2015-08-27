d791127ad6d3f3028ea14d54ce86f374bcfa5a3a
BharathRamakrishnan <Bharath@Shankars-Mac-mini.local>
2015-08-27 14:24:03 +0530

    test

--- YuMeTestApp/Scripts/UnitTest.sh
+++ YuMeTestApp/Scripts/UnitTest.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+# XCTool Setup Path
+XCTOOL_HOME=/usr/local/Cellar/xctool/0.2.4
+export PATH=$XCTOOL_HOME/bin:$PATH
+
 echo "<<<<<<<<<<<<<<<<<<<<<<< UNIT TEST START >>>>>>>>>>>>>>>>>>>>>>>>>>>"
 
 	#xcodebuild -sdk iphonesimulator \
@@ -9,10 +13,13 @@ echo "<<<<<<<<<<<<<<<<<<<<<<< UNIT TEST START >>>>>>>>>>>>>>>>>>>>>>>>>>>"
     #       RUN_APPLICATION_TESTS_WITH_IOS_SIM=YES \
     #       ONLY_ACTIVE_ARCH=NO \
     #       clean build           
+    
+echo $WORKSPACE
+echo $SCHEME_NAME
+echo $TEST_REPORT_FILE
 	
 xctool -workspace $WORKSPACE -scheme $SCHEME_NAME -sdk iphonesimulator -reporter plain -reporter junit:$TEST_REPORT_FILE clean test
     
-#xctool -scheme "$SCHEME_NAME" -sdk iphonesimulator8.4 -configuration Debug -reporter junit:$TEST_REPORT_FILE clean test
 
     #-IDECustomDerivedDataLocation=$BUILD_PATH \
 	#GCC_GENERATE_TEST_COVERAGE_FILES=YES GCC_INSTRUMENT_PROGRAM_FLOW_ARCS=YES \
