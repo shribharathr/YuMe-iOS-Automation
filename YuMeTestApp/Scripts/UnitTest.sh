eda40a7e5be671b71881bc1b6fc64dcab0b7cda2
BharathRamakrishnan <Bharath@Shankars-Mac-mini.local>
2015-08-27 13:12:16 +0530

    script unitest

---
+++ YuMeTestApp/Scripts/UnitTest.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+echo "<<<<<<<<<<<<<<<<<<<<<<< UNIT TEST START >>>>>>>>>>>>>>>>>>>>>>>>>>>"
+
+	#xcodebuild -sdk iphonesimulator \
+    #       -workspace $WORKSPACE \
+    #       -scheme $SCHEME_NAME \
+    #       -configuration Debug \
+    #       RUN_APPLICATION_TESTS_WITH_IOS_SIM=YES \
+    #       ONLY_ACTIVE_ARCH=NO \
+    #       clean build           
+	
+xctool -workspace $WORKSPACE -scheme $SCHEME_NAME -sdk iphonesimulator -reporter plain -reporter junit:$TEST_REPORT_FILE clean test
+    
+#xctool -scheme "$SCHEME_NAME" -sdk iphonesimulator8.4 -configuration Debug -reporter junit:$TEST_REPORT_FILE clean test
+
+    #-IDECustomDerivedDataLocation=$BUILD_PATH \
+	#GCC_GENERATE_TEST_COVERAGE_FILES=YES GCC_INSTRUMENT_PROGRAM_FLOW_ARCS=YES \
+    
+echo "<<<<<<<<<<<<<<<<<<<<<<< UNIT TEST END >>>>>>>>>>>>>>>>>>>>>>>>>>>"
