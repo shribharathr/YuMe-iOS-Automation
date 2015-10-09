10b033537996be58a507546d6d7bc59fc1405497
Bharath <rbharathtech@gmail.com>
2015-10-09 18:30:41 +0530

    iphonesimulator

--- YuMeTestApp/Scripts/UnitTest.sh
+++ YuMeTestApp/Scripts/UnitTest.sh
@@ -17,7 +17,7 @@ echo "<<<<<<<<<<<<<<<<<<<<<<< UNIT TEST START >>>>>>>>>>>>>>>>>>>>>>>>>>>"
 
 #xcodebuild -sdk iphonesimulator -workspace $WORKSPACE/$PWORKSPACE -scheme $SCHEME_NAME -configuration Debug RUN_APPLICATION_TESTS_WITH_IOS_SIM=YES ONLY_ACTIVE_ARCH=NO clean build          
     
-xctool -workspace "$WORKSPACE/$PWORKSPACE" -scheme $SCHEME_NAME -sdk iphonesimulator8.4 -reporter plain -reporter junit:$TEST_REPORT_FILE test
+xctool -workspace "$WORKSPACE/$PWORKSPACE" -scheme $SCHEME_NAME -sdk iphonesimulator -reporter plain -reporter junit:$TEST_REPORT_FILE test
     
     #-IDECustomDerivedDataLocation=$BUILD_PATH \
 	#GCC_GENERATE_TEST_COVERAGE_FILES=YES GCC_INSTRUMENT_PROGRAM_FLOW_ARCS=YES \
