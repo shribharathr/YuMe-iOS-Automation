92ef03a6191c2e5024c1fef4199fe5b6c9805705
Bharath <rbharathtech@gmail.com>
2015-10-09 17:35:26 +0530

    XCTOOL Version

--- YuMeTestApp/Scripts/StaticAnalysis.sh
+++ YuMeTestApp/Scripts/StaticAnalysis.sh
@@ -5,7 +5,7 @@ YUME_TEST_SCHEME=YuMeZISTests
 echo "<<<<<<<<<<<<<<<<<<<<<<< OCLINT ANALYZER START >>>>>>>>>>>>>>>>>>>>>>>>>>>"
 
 # set XCTool PATH 
-XCTOOL_HOME=/usr/local/Cellar/xctool/0.2.4
+XCTOOL_HOME=/usr/local/Cellar/xctool/0.2.5
 export PATH=$XCTOOL_HOME/bin:$PATH
 
 # xctool report with json format
